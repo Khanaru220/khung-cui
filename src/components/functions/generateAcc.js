0d542959149d992471703336938737eea38af476
tin33392js <calsharp0202@yahoo.com>
2022-11-29 23:22:19 +0700

    refactor_nice, more handsome and tươm tất

--- src/components/functions/generateAcc.js
+++ src/components/functions/generateAcc.js
@@ -1,73 +1,44 @@
-import generateRandomRangeNumber from './generateRandomRangeNumber';
+import generateNonrepeatNumbers from './helper/generateNonrepeatNumbers';
+import fetchAPIFilms from './fetchAPIFilms';
 
 class Acc {
 	constructor({ username, password, country, name }) {
-		// 3 input from Sign-up form
 		this.username = username;
 		this.password = password;
-		this._country = country;
 		this.name = name;
+		this._country = country;
 
-		// base on current total number of 'accounts'
-		this.id = Math.round(Math.random() * 10000) + 1;
-		// (later) 'likedFilms' will auto generated 5 films base on 'country'
-		this.friends = []; //hard code from 'initial_5Acc.js'
+		this.id = Math.ceil(Math.random() * 100000);
+		this.friends = [];
+		this.likedFilms = [];
 	}
 	// setter
 	set _country(country) {
 		// trigger, when user add 'country'
 		let filmHolder = [];
-		// temporary -- i need find a way to store 'fetch data' to a varible/property
-
-		const fetchAPIFilmsCountry_3 = async () => {
-			const data = await fetch(
-				`https://api.tvmaze.com/search/shows?q=${country}`,
-				{
-					method: 'GET',
-				}
-			);
-			const shows = await data.json();
-
-			const showsHasImage = shows.filter((showObj) => showObj.show.image);
-			// const randomNumStart = Math.round(
-			// 	Math.random() * (showsHasImage.length - 3)
-			// );
-
-			// take 3 films (smaller if not enough 3)
-			const quantityFilms =
-				showsHasImage.length >= 2 ? 3 : showsHasImage.length;
-			generateRandomRangeNumber(
-				quantityFilms,
-				0,
-				showsHasImage.length - 1
-			).forEach((num) => filmHolder.unshift(showsHasImage[num].show));
-			// (!) API 'search' need read property 'show' to access detail
-		};
-
-		const fetchAPIFilmsRandom_10 = async () => {
-			const data = await fetch(
-				`https://api.tvmaze.com/shows?page=${Math.round(Math.random() * 200)}`,
-				{ method: 'GET' }
-			);
-			const shows = await data.json();
-
-			const quantityFilms = 10;
-			generateRandomRangeNumber(quantityFilms, 0, shows.length - 1).forEach(
-				(num) => filmHolder.push(shows[num])
-			);
-
-			this.likedFilms = filmHolder; // assign after 2 function run
-		};
 
 		// ensure, setter wait until fetching finish
-		// (?) maybe "async" function when declare, will perfom async when call it
-		// (?) or need a way to make 'getter' become async -- need trick
-		const fetchFilms = async () => {
-			await fetchAPIFilmsCountry_3();
-			await fetchAPIFilmsRandom_10();
-		};
-		fetchFilms();
+		// (?) is there a way make 'setter' become async -> A: need trick
+		const fetchAndUpdateLikedFilms = async () => {
+			const quantityOfCountryFilms = 3;
+			const quantityOfBufferFilms = 11;
+			const [countryFilms, bufferFilms] = await Promise.all([
+				fetchAPIFilms({ satisfiedQuantity: 5, country }, 'country'),
+				fetchAPIFilms(),
+			]);
+			// (TODO) create module for hasOnlyImage shows
 
+			[quantityOfCountryFilms, quantityOfBufferFilms].forEach((value, i) => {
+				const quantity = value;
+				const films = i === 0 ? countryFilms : bufferFilms;
+				// pick film random then add to likedFilms
+				generateNonrepeatNumbers(quantity, 0, films.length - 1).forEach(
+					(indexFilm) => this.likedFilms.push(films[indexFilm])
+				);
+			});
+		};
+
+		fetchAndUpdateLikedFilms();
 		this.country = country;
 	}
 }
