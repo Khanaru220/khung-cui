c17448308dccb4b208507c4f135603a5303906f8
tin33392js <calsharp0202@yahoo.com>
2022-11-29 12:16:48 +0700

    rename function more generalized

--- src/components/functions/generateNonrepeatNumbers.js
+++ src/components/functions/generateNonrepeatNumbers.js
@@ -1,5 +1,8 @@
 //  pick film random, instead of linear
 const generateNonrepeatNumbers = (quantity, min = 0, max) => {
+	/* (!) if quantity large, low performace (chance pick unique num harder)
+	 can optimize by split chunks and run Fisher-Yates shuffle
+	 */
 	if (quantity > max - min + 1) {
 		throw new Error('Quantity much smaller than total numbers');
 	}
